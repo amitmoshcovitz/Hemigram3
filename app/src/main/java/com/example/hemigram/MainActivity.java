8f3e5efe282b0b7fc615da4c6123ccf2359eba4f
Amit Moshcovitz <amitmos2005@gmail.com>
2023-06-18 07:52:43 +0300

    UI

--- app/src/main/java/com/example/hemigram/MainActivity.java
+++ app/src/main/java/com/example/hemigram/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.hemigram;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ExpandableListView;
 import android.widget.ImageButton;
+import android.widget.Switch;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,21 +23,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button themeButton = findViewById(R.id.themeButton);
-        themeButton.setOnClickListener(new View.OnClickListener() {
+
+        ImageButton imageButton = findViewById(R.id.settingsButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Toggle the theme between light and dark
-                int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
-                if (currentNightMode == Configuration.UI_MODE_NIGHT_YES) {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                } else {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                }
+                // Create an intent to start the NewActivity
+                Intent intent = new Intent(MainActivity.this, Settings.class);
+                startActivity(intent);
             }
         });
-
-
 
     }
 }
--- app/src/main/java/com/example/hemigram/Settings.java
+++ app/src/main/java/com/example/hemigram/Settings.java
@@ -1,8 +1,12 @@
 package com.example.hemigram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class Settings extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
+        androidx.appcompat.widget.SwitchCompat darkmodeSwitch = findViewById(R.id.darkmodeSwitch);
+        darkmodeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Toggle the theme between light and dark
+                int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+                if (isChecked) {
+                    if (currentNightMode != Configuration.UI_MODE_NIGHT_YES) {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                        recreate(); // Restart the activity to apply the new theme
+                    }
+                } else {
+                    if (currentNightMode != Configuration.UI_MODE_NIGHT_NO) {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                        recreate(); // Restart the activity to apply the new theme
+                    }
+                }
+            }
+        });
     }
 }
